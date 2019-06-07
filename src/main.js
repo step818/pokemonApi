20f79abcd3621ec5bf82d34e17116e6447491423
Stephen Trewick <stephent818@gmail.com>
2019-06-07 03:37:05 -0700

    name and id parameter

--- src/main.js
+++ src/main.js
@@ -9,12 +9,11 @@ $(document).ready(function() {
     console.log(name);
       let pokemon = new Pokedex();  // create instance of WeatherService class
       let promise = pokemon.getPokemon(name);  // call the instance method and pass in user input
-    console.log(pokemon);
       promise.then(function(response) {
       let body = JSON.parse(response);
-        console.log(body.cards.name);
-        $('.showCard').text(`The pokemon ${name} is ${body.cards.name}`);
-        //$('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+        console.log(body.id);
+        $('.showCard').text(`The pokemon ${name} is ${body.name}`);
+        $('.showTemp').text(`The id of your pokemon is ${body.id}`);
       }, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
       });
--- src/pokemon.js
+++ src/pokemon.js
@@ -2,7 +2,7 @@ export class Pokedex {
     getPokemon(name) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        let url = `https://api.pokemontcg.io/v1/cards?name=${name}`;
+        let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
         request.onload = function() {
           if (this.status === 200) {
             resolve(request.response);
