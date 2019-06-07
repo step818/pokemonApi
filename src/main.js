20826d6a9a9cb8cab8719467e0995fb6658eaeac
Stephen Trewick <stephent818@gmail.com>
2019-06-07 04:19:01 -0700

    few more params

--- src/main.js
+++ src/main.js
@@ -11,9 +11,11 @@ $(document).ready(function() {
       let promise = pokemon.getPokemon(name);  // call the instance method and pass in user input
       promise.then(function(response) {
       let body = JSON.parse(response);
-        console.log(body.id);
+        console.log(body.moves[1].move.name);
         $('.showCard').text(`The pokemon ${name} is ${body.name}`);
-        $('.showTemp').text(`The id of your pokemon is ${body.id}`);
+        $('.showTemp').text(`The id of your pokemon is ${body.id} and weight is ${body.weight}`);
+        console.log(body.moves.length);
+        $('.showMoves').text(`The moves are ${body.moves[1].move.name}`);
       }, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
       });
